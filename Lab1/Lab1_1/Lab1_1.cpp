a2888ff42fc239efa4bd3038d070bb185bc775d7
Dmitriy Khomos <69244728+dimensenss@users.noreply.github.com>
2021-09-22 21:40:14 +0300

    Add files via upload

---
+++ Lab1/Lab1_1/Lab1_1.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int a1 = sizeof(int);
+    int a2 = sizeof(short);
+    int a3 = sizeof(long);
+    int a4 = sizeof(char);
+    int a5 = sizeof(float);
+    int a6 = sizeof(double);
+}
