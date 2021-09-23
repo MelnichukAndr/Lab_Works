08e6889b6786d40751bc6218f8a5464012750e3c
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-09-24 01:29:03 +0300

    Lab1_6 in procces

---
+++ Lab1/Lab1_6/Lab1_6.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	const int sec_In_Min = 60;
+	int sec_In_Hour = 60 * sec_In_Min;
+	int sec_In_Day = 24 * sec_In_Hour;
+	int sec_In_Year = 365 * sec_In_Day;
+	long long sec_In_100_Years = 100 * sec_In_Year; // переполнение
+	long long sec_In_1000_Years = 1000 * sec_In_Year; // переполнение
+}
+
