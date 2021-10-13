b9c64b5e751c6c0f1ae0001b4eae11803f1b4c0d
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-10-13 11:09:20 +0300

    ok

---
+++ Lab5/Lab5_1/Lab5_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    short k;
+    printf("\nВведите k >");
+    scanf_s("%hd", &k);
+    if (k % 10 == 1)
+        printf("У меня %hd гриб\n", k);
+    if (k % 10 == 5)
+        printf("У меня %hd грибов\n", k);
+
+
+
+
+
+
+
+
+}
+
