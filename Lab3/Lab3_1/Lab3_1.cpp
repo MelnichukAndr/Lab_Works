1e96fde3dd04f717eb8c5046eca0245eb8e02c1f
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-10-13 13:27:03 +0300

    ok

--- Lab3/Lab3_1/Lab3_1.cpp
+++ Lab3/Lab3_1/Lab3_1.cpp
@@ -9,20 +9,20 @@ int main(void)
     double x;
     double ax, ab;
     double b;
-    
+    while (1) {
         printf("\n");
         printf("Ведите значение x (1;4) >\t");
-        scanf("%lf" , &x);
-        printf("Введите значение b (3;10) >\t");
+        scanf("%lf", &x);
+        printf("Введите значение b (5;10) >\t");
         scanf("%lf", &b);
 
         ax = a * x;
         ab = a * b;
 
-        if (x <=4 & b>=3) {
+        if (x <= 4 & b >= 3) {
             t1 = 1 / (6 * pow(a, 3)) * log((a + x) / (a - x)) + 1 / (2 * pow(a, 3));
             printf("t1=%lg\n", t1);
-            
+
             t2 = (1 / ab) * log(tan(ax + b) / tan(ax - b));
             printf("t2=%lg\n", t2);
         }
@@ -30,8 +30,8 @@ int main(void)
             printf("EROR!\n");
             printf("Введите значение x или y в пределах\n");
         }
-    
-   
+
+    }
         
 
         /*t1 = 1 / (6 * pow(a, 3)) * log((a + x) / (a - x)) + 1 / (2 * pow(a, 3));
--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -9,21 +9,21 @@ int main()
     setlocale(LC_ALL, "ru");
 
     double x, y;
+    while (1) {
+        printf("Введите значение x >\n");
+        scanf_s("%lf", &x);
 
-    printf("Введите значение x >\n");
-    scanf_s("%lf", &x);
+        printf("Введите значение y >\n");
+        scanf_s("%lf", &y);
 
-    printf("Введите значение y >\n");
-    scanf_s("%lf", &y);
-
-    printf("x=%6.3lf;  y=%6.3lf\n", x, y);
+        printf("x=%6.3lf;  y=%6.3lf\n", x, y);
 
 
-    if (((x * x + y * y )<= 1 && y <= 0 && x <= 0) || (x >= 0 && x <= 1 && y >= 0 && y <= 1))
-        printf("Точка попадает в область\n");
-    else
-        printf("Точка не попадает в область\n");
-
+        if (((x * x + y * y) <= 1 && y <= 0 && x <= 0) || (x >= 0 && x <= 1 && y >= 0 && y <= 1))
+            printf("Точка попадает в область\n");
+        else
+            printf("Точка не попадает в область\n");
+    }
     
 }
 
