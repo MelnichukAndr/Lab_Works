8bfee5adeeec8aa92f8dc937bc6ba74d7cb0427c
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-10-13 12:07:18 +0300

    Lab5_1 completed

--- Lab5/Lab5_1/Lab5_1.cpp
+++ Lab5/Lab5_1/Lab5_1.cpp
@@ -1,25 +1,38 @@
-﻿#include <stdio.h>
-#include <iostream>
+﻿
 #define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
+int main() {
+	setlocale(LC_ALL, "RUS");
 
-int main()
-{
-    setlocale(LC_ALL, "ru");
-    short k;
-    printf("\nВведите k >");
-    scanf_s("%hd", &k);
-    if (k % 10 == 1)
-        printf("У меня %hd гриб\n", k);
-    if (k % 10 == 5)
-        printf("У меня %hd грибов\n", k);
+	int grib(0);
 
+	printf("\t***Склонение слова гриб в зависимости от числа***\n");
+	printf("Введите количество грибов >");
+	scanf("%d", &grib);
 
+		if ((grib % 100) >= 11 && (grib % 100) <= 19)
+				cout << grib << " грибов";
 
+			else if (grib % 10 >= 2 && grib % 10 <= 4)
+				cout << grib << " гриба";
 
+		    else if (grib % 10 == 1)
+		        cout << grib << " гриб";
 
+		    else if (grib % 10 >= 5 && grib % 10 <= 9 || grib % 10 == 0)
+		        cout << grib << " грибов";
+
+		    else
+		        cout << "Ошибка, введите положительное число!";
+
+		printf("\n\n\n\n\n");
+	
+	return 0;
+		
 
 
 
 }
-
