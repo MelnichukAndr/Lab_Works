34ba5e1e98d039830238350df184af07285d4d8a
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-10-13 14:46:32 +0300

    ok

--- Lab5/Lab5_1/Lab5_1.cpp
+++ Lab5/Lab5_1/Lab5_1.cpp
@@ -2,37 +2,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
+#include <conio.h>
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "RUS");
 
 	int grib(0);
-
-	printf("\t***Склонение слова гриб в зависимости от числа***\n");
-	printf("Введите количество грибов >");
-	scanf("%d", &grib);
+	while (1) {
+		system("cls");
+		printf("\t***Склонение слова гриб в зависимости от числа***\n");
+		printf("Введите количество грибов >");
+		scanf("%d", &grib);
 
 		if ((grib % 100) >= 11 && (grib % 100) <= 19)
-			cout << grib << " грибов";
+			printf("У меня %d грибов", grib);
 
-		    else if (grib % 10 >= 2 && grib % 10 <= 4)
-			cout << grib << " гриба";
+		else if (grib % 10 >= 2 && grib % 10 <= 4)
+			printf("У меня %d гриба", grib);
 
-		    else if (grib % 10 == 1)
-		        cout << grib << " гриб";
+		else if (grib % 10 == 1)
+			printf("У меня %d гриб", grib);
 
-		    else if (grib % 10 >= 5 && grib % 10 <= 9 || grib % 10 == 0)
-		        cout << grib << " грибов";
+		else if (grib % 10 >= 5 && grib % 10 <= 9 || grib % 10 == 0)
+			printf("У меня %d грибов", grib);
 
-		    else
-		        cout << "Ошибка, введите положительное число!";
+		else
+			printf("Ошибка, введите положительное число!");
+
+
+
 
 		printf("\n\n\n\n\n");
-	
+		printf("Введите любую клавишу для продолжения");
+		_getch();
+	}
+
 	return 0;
-		
-
-
-
 }
