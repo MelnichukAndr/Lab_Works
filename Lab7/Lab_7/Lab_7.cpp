9010a3fe9ff81f095e5adce21f365447829ad1ab
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-10-27 12:43:40 +0300

    ок

---
+++ Lab7/Lab_7/Lab_7.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+int main() {
+    int n;
+    double x, y;
+    short h;
+
+    for (n = 0; n < 5; n++) {
+
+        printf("|   x   |      y     |\n");
+        printf("|-------|------------|\n");
+        /* внутренний цикл для одного периода */
+        for (x = 0; x < 4; x += 0.25) {
+            /* 1-й отрезок */
+            if (x <= 2) y = sqrt(4 - (x - 2) * (x - 2));
+            /* 2-й отрезок */
+            else if (x < 4) y = -x + 4;
+
+            printf("| %5.2%lf | %10.7%lf |", x + n * 4, y);
+            /* определение позиции точки */
+            h = (y + 1) * 10;
+            if (y - 1 - h * 10 + 0.5) h++;
+            /* вывод точки графика */
+            for (; h > 0; h--) printf(" ");
+            printf("*\n");
+
+            printf("Нажмите клавишу Enter...");
+            getchar();
+
+            return 0;
+        }
+    }
+}
