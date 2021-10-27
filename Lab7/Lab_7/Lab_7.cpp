2ea3a6411618b6d95725933bf1352eb3826dc2c9
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-10-27 13:12:15 +0300

    ok

--- Lab7/Lab_7/Lab_7.cpp
+++ Lab7/Lab_7/Lab_7.cpp
@@ -1,7 +1,8 @@
 ﻿#include <stdio.h>
 #include <math.h>
 #include <iostream>
-int main() {
+int main(void) {
+    setlocale(LC_ALL, "RUS");
     int n;
     double x, y;
     short h;
@@ -13,14 +14,14 @@ int main() {
         /* внутренний цикл для одного периода */
         for (x = 0; x < 4; x += 0.25) {
             /* 1-й отрезок */
-            if (x <= 2) y = sqrt(4 - (x - 2) * (x - 2));
+            if (x < 2) y = sqrt(4 - (x - 2) * (x - 2));
             /* 2-й отрезок */
             else if (x < 4) y = -x + 4;
 
-            printf("| %5.2%lf | %10.7%lf |", x + n * 4, y);
+            printf("| %5.2lf | %10.7lf |", x + n * 4, y);
             /* определение позиции точки */
             h = (y + 1) * 10;
-            if (y - 1 - h * 10 + 0.5) h++;
+            if (y - 1 - h * 10 > 0.5) h++;
             /* вывод точки графика */
             for (; h > 0; h--) printf(" ");
             printf("*\n");
