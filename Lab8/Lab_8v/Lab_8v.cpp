dc7552331bac358fb8b7a45c1d5e881ee989c5cb
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-11-12 15:28:23 +0200

    ok

---
+++ Lab8/Lab_8v/Lab_8v.cpp
@@ -0,0 +1,61 @@
+﻿
+#include <iostream>
+#include <string>
+
+    using namespace std;
+
+    int main()
+    {
+        int number;//введенное число
+        int const N = 11;
+
+
+        int Arr[N];
+
+
+        int n, count_of_digits, sum, k;
+
+        number = count_of_digits = sum = 0;
+        int i = 0;
+
+        cout << "Enter number" << endl;
+        cin >> number;
+        k = number;
+        if (number < 2147483647) {
+           
+
+
+                while (number > 0) {
+                    Arr[i] = number % 10;
+                    number /= 10;
+                    count_of_digits++;
+                    i++;
+                }
+
+
+                for (i = 0; i < count_of_digits; i++) {
+                    sum = sum + Arr[i];
+                }
+
+
+                /* int n = 123;*/
+                string s = to_string(k);
+                string s_reverse(s);
+                reverse(s_reverse.begin(), s_reverse.end());
+
+
+
+                cout << "Sum = " << sum << endl;
+                cout << "Count of digits = " << count_of_digits << endl;
+                cout << "Reverse = " << s_reverse << endl;
+                /*  system("pause");*/
+                return 0;
+            }
+        
+
+       
+            cout << "Enter number in the range of values for int type";
+        }
+    
+        
+    
---
+++ Lab9/Lab9/Lab9.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#define N 9
+
+int main()
+{
+    int spiral[N][N];
+    int i, j, k, edge, num = N * N;
+    for (k = 0; k < N / 2; k++) //слои
+    {
+        edge = N - 1 - k;
+        for (j = k; j < edge; j++, num--)
+            spiral[k][j] = num;
+        for (i = k; i < edge; i++, num--)
+            spiral[i][edge] = num;
+        for (j = edge; j > k; j--, num--)
+            spiral[edge][j] = num;
+        for (i = edge; i > k; i--, num--)
+            spiral[i][k] = num;
+    }
+
+    if (N % 2) //если N-нечетное дополнить центральным элементом
+        spiral[N / 2][N / 2] = num;
+
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++) printf("%6d", spiral[i][j]);
+        printf("\n");
+    }
+
+  
+    return 0;
+}
