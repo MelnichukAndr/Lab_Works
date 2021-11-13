196ffc406e86118e42d5c6a1a2101aba49f61f50
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2021-11-13 17:30:40 +0200

    Add correct cout

--- Lab9/Lab9/Lab9.cpp
+++ Lab9/Lab9/Lab9.cpp
@@ -1,35 +1,42 @@
 ﻿#include <stdio.h>
-#include <conio.h>
 #include <iostream>
+#include <iomanip>
 #define N 9
+using namespace std;
 
-int main()
-{
-    int spiral[N][N];
+int main(){
+
+    int arr[N][N];
+
     int i, j, k, edge, num = N * N;
-    for (k = 0; k < N / 2; k++) //слои
-    {
+
+    for (k = 0; k < N / 2; k++) { //слои
+
         edge = N - 1 - k;
-        for (j = k; j < edge; j++, num--)
-            spiral[k][j] = num;
-        for (i = k; i < edge; i++, num--)
-            spiral[i][edge] = num;
-        for (j = edge; j > k; j--, num--)
-            spiral[edge][j] = num;
+
         for (i = edge; i > k; i--, num--)
-            spiral[i][k] = num;
+            arr[i][k] = num;
+
+        for (j = k; j < edge; j++, num--)
+            arr[k][j] = num;
+
+        for (i = k; i < edge; i++, num--)
+            arr[i][edge] = num;
+
+        for (j = edge; j > k; j--, num--)
+            arr[edge][j] = num;
     }
 
     if (N % 2) //если N-нечетное дополнить центральным элементом
-        spiral[N / 2][N / 2] = num;
+        arr[N / 2][N / 2] = num;
 
+     for (i = 0; i < N; i++){
 
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++) printf("%6d", spiral[i][j]);
-        printf("\n");
-    }
+            for (j = N - 1; j >= 0; j--) {
 
-  
+                cout << setw(6) << arr[i][j];
+            }
+        cout << endl;
+     }
     return 0;
 }
