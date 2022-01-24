845bccb4530a948e8209f4d3eadafaabdad9d8f3
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2022-01-24 19:16:39 +0200

    ok

--- Lab11/Lab11/Lab11.cpp
+++ Lab11/Lab11/Lab11.cpp
@@ -1,33 +1,58 @@
 ﻿#include <iostream>
 #include <windows.h>
+#define _CRT_SECURE_NO_WARNINGS
+
 using namespace std;
 
-void foo(int i, int s)
+void foo(int i, int s, int arr[])
 {
-    if (i == 0)
-        cout << 0;
-    else {
-        if (i / s > 0) {
-            foo(i / s, s);
-        }
-        cout << i % s;
-    }
+	static int j = 0;
+	if (i == 0)
+		cout << 0;
+	else
+	{
+		if (i / s > 0)
+		{
+			foo(i / s, s, arr);
+		}
+		arr[j] = i % s;
+		j++;
+	}
 }
-int main() 
+int main()
 {
-    setlocale(LC_ALL, "Russian");
-    int i;
-    while (true) 
-    {
-        cout << "Введите число в десятичной СС: ";
-        cin >> i;
+	setlocale(LC_ALL, "Russian");
+	int i = 0;
+	int k = 0;
+	int_fast64_t m = 0;
+	int stepen = 0;
+	int p = 0;
+	int arr[12];
 
-        cout << "Число " << i << " в " << 8 << " CC: "; foo(i, 8);
-    
-        cout << endl << endl;
-        system("pause");
-        system("cls");
-    }
+	cout << "Введите число в десятичной СС: ";
+	cin >> i;
+	p = i;
+
+	if (i >= 32768)
+	{
+		stepen = 1;
+	}
+
+	while (p > 0)
+	{
+		p % 10;
+		p /= 10;
+		stepen++;
+	}
+
+	foo(i, 8, arr);
+
+	for (int i = 0; i < stepen; i++)
+	{
+		m += arr[i] * pow(10, stepen - 1 - i);
+	}
+	cout << "Число " << i << " в " << 8 << " CC:  " << m;
+
+	cout << endl << endl;
+	system("pause");
 }
-
-
