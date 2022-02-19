860b3539dd543d2153b1d529661b35323bd2d930
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2022-02-19 20:09:32 +0200

    ok

---
+++ Labs  (2 semester)/Lab 3 (2)/ConsoleApplication1/Lab 3 (2).cpp
@@ -0,0 +1,35 @@
+﻿#include "fun.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+	int size;
+
+	cout << "Введите размер массива в диапозоне 1-100 " << endl;
+	cin >> size;
+	if (size < 1 || size>100) cout << "Введите размер в диапозоне 1-100";
+
+	int* arr = new int[size];
+	int* brr = new int[3];
+
+	fill(arr, size);
+	print(arr, size);
+	sort(arr, size);
+
+	cout << "\n";
+
+	print(arr, size); //sort arr
+
+	cout <<"\n\n\n\n";
+
+	barr(arr, brr); // fill brr
+	print(brr, 3);
+
+	cout <<"\n";
+
+	delete[] arr;
+	delete[] brr;
+}
