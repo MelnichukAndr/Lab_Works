d73cc185d3ee794e621a0de9278363afa382a7c9
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2022-02-22 21:47:14 +0200

    ol

--- Labs  (2 semester)/Lab 3a/Lab 3a/Lab 3a.cpp
+++ Labs  (2 semester)/Lab 3a/Lab 3a/Lab 3a.cpp
@@ -1,20 +1,152 @@
-﻿// Lab 3a.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include "fun.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+
+	int size;
+	int variant;
+	cout << "Введите размер массива" << endl;
+	cin >> size;
+
+	int* arr = new int[size];
+
+	int* null_ptr = arr;
+
+
+	int sum, c = 0;
+
+	fill(arr, size);
+	do
+	{
+		print_menu();
+
+		variant = get_variant();
+		switch (variant)
+		{
+
+		case 1:
+		{
+			system("cls");
+			arr = null_ptr;
+			cout << "Относительный адрес ";
+			print(arr, size);
+			sum = 0, c = 0;
+			for (int i = 0; i < size; i++)
+			{
+				if (*(arr + i) < 0)
+					sum += *(arr + i), c++;
+			}
+			cout << "Сумма отрицательных элементов: " << sum << endl;
+			cout << "Кол-во отрицательных элементов: " << c << endl;
+			system("pause");
+			system("cls");
+		}break;
+
+		case 2:
+		{
+			system("cls");
+			arr = null_ptr;
+			print(arr, size);
+			cout << endl << "Абсолютный адрес" << endl;
+			sum = 0, c = 0;
+			for (int i = 0; i < size; i++)
+			{
+				if (*arr < 0)
+					sum += *arr, c++;
+				arr++;
+			}
+			cout << "Сумма отрицательных элементов: " << sum << endl;
+			cout << "Кол-во отрицательных элементов: " << c << endl;
+			system("pause");
+			system("cls");
+		}break;
+
+		case 3:
+		{
+			system("cls");
+			arr = null_ptr;
+			print(arr, size);
+			cout << endl << "Без использования адресации: " << endl;
+			sum = 0, c = 0;
+			for (arr = null_ptr; arr < null_ptr + size; arr++)
+			{
+				if (*arr < 0)
+					sum += *arr, c++;
+			}
+			cout << "Сумма отрицательных элементов: " << sum << endl;
+			cout << "Кол-во отрицательных элементов: " << c << endl;
+			system("pause");
+			system("cls");
+		}break;
+
+		case 4:
+		{
+			system("cls");
+			arr = null_ptr;
+			print(arr, size);
+			cout << endl << "Через массив указателей: " << endl;
+			sum = 0, c = 0;
+			int* ptr_arr[100];
+			for (int i = 0; i < size; i++)
+			{
+				ptr_arr[i] = &arr[i];
+			}
+			for (int i = 0; i < size; i++)
+			{
+				if (*ptr_arr[i] < 0)
+					sum += (*ptr_arr[i]), c++;
+			}
+			cout << "Сумма отрицательных элементов: " << sum << endl;
+			cout << "Кол-во отрицательных элементов: " << c << endl;
+			system("pause");
+			system("cls");
+		}break;
+
+		case 5:
+		{
+			system("cls");
+			arr = null_ptr;
+			print(arr, size);
+			cout << endl << "Указатель на указатель" << endl;
+			sum = 0, c = 0;
+			int** ptr_ptr;
+			for (int i = 0; i < size; i++)
+			{
+				ptr_ptr = &arr;
+				if (**ptr_ptr < 0)
+					sum += **ptr_ptr, c++;
+				arr++;
+			}
+			cout << "Сумма отрицательных элементов: " << sum << endl;
+			cout << "Кол-во отрицательных элементов: " << c << endl;
+			system("pause");
+			system("cls");
+
+		}break;
+
+		case 6:
+		{
+			system("cls");
+			arr = null_ptr;
+			print(arr, size);
+			cout << endl << "В виде функции с параметрами указателями" << endl;
+			sum = 0, c = 0;
+			foo(arr, size, &sum, &c);
+			cout << "Сумма отрицательных элементов: " << sum << endl;
+			cout << "Кол-во отрицательных элементов: " << c << endl;
+			system("pause");
+			system("cls");
+		}break;
+
+		if (variant != 7)
+			system("pause");
+		}
+
+	} while (variant != 7);
+
+	delete[] arr;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
