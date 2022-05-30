78c24fdfdfbd51a530802ffce20c3a673c7261af
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2022-05-30 17:22:24 +0300

    ok

---
+++ Labs  (2 semester)/Lab 10a/Lab 10a/Lab 10a.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+class figure{
+public:	
+    int Inc(int a = 2, int b = 0)
+    {
+        if (a == 0)
+            return b * b;
+        if(b == 0)
+            return a * a;
+    return a * b;
+    }
+};
+
+int main()
+{
+    figure area;
+    cout << "Figure area = " << area.Inc() << "\n\n\n";
+}
---
+++ Labs  (2 semester)/Lab 10b/Lab 10b/Lab 10b.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+class replaceValues
+{
+private:
+	void swap(int& a, int& b)
+	{
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+public:
+	void Swap(int& a, int& b, int& c)
+	{
+		int min, max, temp;
+		min = a; max = a;
+		if (b < min)
+			min = b;
+		
+		if (c < min) 
+			min = c;
+		
+		if (b > max) 
+			max = b;
+		
+		if (c > max)
+			max = c;
+		
+		if (((a == max) && (b == min)) || 
+			((a == min) && (b == max))) 
+			swap(a, b);
+		
+		if (((b == max) && (c == min)) || 
+			((b == min) && (c == max))) 
+			swap(b, c);
+		
+		if (((a == max) && (c == min)) || 
+			((a == min) && (c == max))) 
+			swap(a, c);
+		
+	}
+	void Swap(int* a, int* b, int* c)
+	{
+		int min, max, temp;
+		min = *a; max = *a;
+		if (*b < min)
+			min = *b;
+		
+		if (*c < min)
+			min = *c;
+		
+		if (*b > max)
+			max = *b;
+		
+		if (*c > max)
+			max = *c;
+		
+		if (((*a == max) && (*b == min)) || 
+			((*a == min) && (*b == max))) 
+			swap(*a, *b);
+		
+		if (((*b == max) && (*c == min)) || 
+			((*b == min) && (*c == max))) 
+			swap(*b, *c);
+		
+		if (((*a == max) && (*c == min)) || 
+			((*a == min) && (*c == max))) 
+			swap(*a, *c);
+	}
+};
+int main()
+{
+	replaceValues value;
+	int a, b, c;
+	do
+	{
+		cout << "Enter a, b, c\n";
+		cin >> a >> b >> c;
+		system("cls");
+		cout << "Before swap:\na = " << a << " b = " << b << " c = " << c << endl;
+		value.Swap(&a, &b, &c);
+		cout << "After swap by link:\na = " << a << " b = " << b << " c = " << c << endl;
+		cout << "\n";
+		cout << "Before swap:\na = " << a << " b = " << b << " c = " << c << endl;
+		value.Swap(a, b, c);
+		cout << "After swap by pointers:\na = " << a << " b = " << b << " c = " << c << endl;
+		system("pause");
+		system("cls");
+	} while (_getch() != 27);
+}
+
