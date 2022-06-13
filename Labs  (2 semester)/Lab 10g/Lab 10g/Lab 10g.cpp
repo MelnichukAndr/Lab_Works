d3b4423488235c77ff3cbb9ce8d5afc36da909bf
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2022-06-13 11:16:10 +0300

    ok

---
+++ Labs  (2 semester)/Lab 10g/Lab 10g/Lab 10g.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+class timer {
+	int sec;
+	public:
+	timer(int sec) {
+		this->sec = sec;
+		cout << "constructor " << this << endl;
+	}
+	timer(int sec, int min) {
+		this->sec = sec + min * 60;
+		cout << "constructor " << this << endl;
+	}
+	timer(const timer& other) {
+		this->sec = other.sec;
+		cout << "constructor " << this << endl;
+	}
+	
+	void print() {
+		cout << "sec = " << sec<<endl;
+	}
+	~timer() {
+		cout <<"destructor " << this << endl;
+	}
+	
+};
+
+int main()
+{
+	timer FirstValue(30);
+	timer SecondValue(10, 1);
+	timer ThirdValue(FirstValue);
+	FirstValue.print();
+	SecondValue.print();
+	ThirdValue.print();
+}
