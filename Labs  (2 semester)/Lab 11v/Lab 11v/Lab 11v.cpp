ece3c6c0022858edaf727bc34b3e45c9b8181d22
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2022-06-13 12:46:13 +0300

    ok

---
+++ Labs  (2 semester)/Lab 11v/Lab 11v/Lab 11v.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+class complex {
+	double r, m;
+	void AddR(int r) {
+		this->r += r;
+	}
+	void AddM(int m) {
+		this->m += m;
+	}
+public:
+	complex() {
+		r = 0;
+		m = 0;
+	}
+	complex(double r, double m) {
+		this->r = r;
+		this->m = m;
+	}
+	void SetR(double r) {
+		this->r = r;
+	}
+	void SetM(double m) {
+		this->m = m;
+	}
+	void Sum(const complex& other) {
+		AddR(other.r);
+		AddM(other.m);
+	}
+	void print() {
+		cout << r << " + " << m << "i" << endl;
+	}
+};
+	
+int main()
+{
+	complex FirstComplex;
+	FirstComplex.SetR(5);
+	FirstComplex.SetM(3);
+	complex SecondComplex;
+	SecondComplex.SetR(2);
+	SecondComplex.SetM(6);
+	FirstComplex.Sum(SecondComplex);
+	FirstComplex.print();
+}
